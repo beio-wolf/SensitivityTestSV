63f93efd096dbc05f51978fd625299f257810b49
Kunihiko Mori <kunihiko_mori@beio-wolf.com>
2018-04-12 07:55:37 +0900

    距離算出計算を実装

--- SensitivitySV/ConfarmFarViewController.swift
+++ SensitivitySV/ConfarmFarViewController.swift
@@ -10,10 +10,45 @@ import UIKit
 
 class ConfarmFarViewController: UIViewController {
 
+    @IBOutlet weak var distanceLabel_: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+        // 距離を算出
+        let dataServer = DataServer.shared
+
+        let S_old_R:Float = dataServer.getCurrentPrescriptionR_S()
+        let C_old_R:Float = dataServer.getCurrentPrescriptionR_C()
+        let S_old_L:Float = dataServer.getCurrentPrescriptionL_S()
+        let C_old_L:Float = dataServer.getCurrentPrescriptionL_C()
+        
+        let S_new_R:Float = dataServer.getNewestPrescriptionR_S()
+        let C_new_R:Float = dataServer.getNewestPrescriptionR_C()
+        let S_new_L:Float = dataServer.getNewestPrescriptionL_S()
+        let C_new_L:Float = dataServer.getNewestPrescriptionL_C()
+
+        let S_old:Float = (S_old_R + S_old_L) / 2.0
+        let S_new:Float = (S_new_R + S_new_L) / 2.0
+        let C_old:Float = (C_old_R + C_old_L) / 2.0
+        let C_new:Float = (C_new_R + C_new_L) / 2.0
+        
+        let F_old:Float = S_old + (C_old / 2.0)
+        let F_new:Float = S_new + (C_new / 2.0)
+
+        let G_f:Float = F_new - F_old
+        
+        var distance:Float = 1.0 / (1.0 + G_f)
+        if (distance >= 1.5) {
+            distance = 1.5
+        } else if (distance <= 0.5) {
+            distance = 0.5
+        }
+        
+        // 距離を表示
+        distanceLabel_.text = String(distance) + "m"
     }
 
     override func didReceiveMemoryWarning() {
--- SensitivitySV/ConfarmNearViewController.swift
+++ SensitivitySV/ConfarmNearViewController.swift
@@ -10,10 +10,45 @@ import UIKit
 
 class ConfarmNearViewController: UIViewController {
 
+    @IBOutlet weak var distanceLabel_: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // 距離を算出
+        let dataServer = DataServer.shared
+        
+        let S_old_R:Float = dataServer.getCurrentPrescriptionR_S()
+        let C_old_R:Float = dataServer.getCurrentPrescriptionR_C()
+        let S_old_L:Float = dataServer.getCurrentPrescriptionL_S()
+        let C_old_L:Float = dataServer.getCurrentPrescriptionL_C()
+        
+        let S_new_R:Float = dataServer.getNewestPrescriptionR_S()
+        let C_new_R:Float = dataServer.getNewestPrescriptionR_C()
+        let S_new_L:Float = dataServer.getNewestPrescriptionL_S()
+        let C_new_L:Float = dataServer.getNewestPrescriptionL_C()
+        
+        let S_old:Float = (S_old_R + S_old_L) / 2.0
+        let S_new:Float = (S_new_R + S_new_L) / 2.0
+        let C_old:Float = (C_old_R + C_old_L) / 2.0
+        let C_new:Float = (C_new_R + C_new_L) / 2.0
+        
+        let N_old:Float = S_old + (C_old / 2.0)
+        let N_new:Float = S_new + (C_new / 2.0)
+        
+        let G_n:Float = N_new - N_old
+        
+        var distance:Float = 100.0 / (2.5 + G_n)
+        if (distance >= 50.0) {
+            distance = 50.0
+        } else if (distance <= 30.0) {
+            distance = 30.0
+        }
+        
+        // 距離を表示
+        distanceLabel_.text = String(distance) + "m"
     }
 
     override func didReceiveMemoryWarning() {
