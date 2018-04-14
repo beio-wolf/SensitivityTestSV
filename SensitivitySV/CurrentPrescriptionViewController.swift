65052f3ee8a916f9bae3ca1196e401d0daa109b7
Kunihiko Mori <kunihiko_mori@beio-wolf.com>
2018-04-14 12:48:31 +0900

    ・ドラムロールの選択肢に「-0」を追加した。

--- SensitivitySV/CurrentPrescriptionViewController.swift
+++ SensitivitySV/CurrentPrescriptionViewController.swift
@@ -13,8 +13,8 @@ let currentPrescriptionPickerView1 = UIPickerView()
 let currentPrescriptionPickerView2 = UIPickerView()
 let currentPrescriptionPickerView3 = UIPickerView()
 
-var currentIntDataArray = ["+10", "+9", "+8", "+7", "+6", "+5", "+4", "+3", "+2", "+1", "0",
-                           "-1", "-2", "-3", "-4", "-5", "-6", "-7", "-8", "-9", "-10",
+var currentIntDataArray = ["+10", "+9", "+8", "+7", "+6", "+5", "+4", "+3", "+2", "+1", "+0",
+                           "-0", "-1", "-2", "-3", "-4", "-5", "-6", "-7", "-8", "-9", "-10",
                            "-11", "-12", "-13", "-14", "-15", "-16"]
 var currentDecimalDataArray = ["00", "25", "50", "75"]
 
--- SensitivitySV/NewestPrescriptionViewController.swift
+++ SensitivitySV/NewestPrescriptionViewController.swift
@@ -13,8 +13,8 @@ let prescriptionPickerView1 = UIPickerView()
 let prescriptionPickerView2 = UIPickerView()
 let prescriptionPickerView3 = UIPickerView()
 
-var intDataArray = ["+10", "+9", "+8", "+7", "+6", "+5", "+4", "+3", "+2", "+1", "0",
-                    "-1", "-2", "-3", "-4", "-5", "-6", "-7", "-8", "-9", "-10",
+var intDataArray = ["+10", "+9", "+8", "+7", "+6", "+5", "+4", "+3", "+2", "+1", "+0",
+                    "-0", "-1", "-2", "-3", "-4", "-5", "-6", "-7", "-8", "-9", "-10",
                     "-11", "-12", "-13", "-14", "-15", "-16"]
 var decimalDataArray = ["00", "25", "50", "75"]
 
