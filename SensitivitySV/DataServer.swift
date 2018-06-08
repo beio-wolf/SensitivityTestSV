a6ebec1bf6a6845f8b85c389efd23807722f2181
森 邦彦 <kunihiko_mori@beio-wolf.com>
2018-06-08 15:06:10 +0900

    中国語２のボタン画像を差し替え。

--- SensitivitySV/DataServer.swift
+++ SensitivitySV/DataServer.swift
@@ -38,6 +38,19 @@ enum SettingPos {
     case Module // 非共通
 }
 
+// 国
+enum Country:Int {
+    case Japan      // 日本
+    case Other      // その他
+}
+
+// 試験種別
+enum TestKind {
+    case Near   // 近視
+    case Far    // 遠視
+}
+
+
 final class DataServer {
     
     // シングルトン
@@ -50,21 +63,34 @@ final class DataServer {
     private var region_:Region          = Region.US
     private var lensType_:LensType      = LensType.SingleVision
     private var settingPos_:SettingPos  = SettingPos.Common
+    private var country_:Country        = Country.Japan
+    private var testKind_:TestKind      = TestKind.Far
 
-    private var comfirmIndex_:Int    = 0
-    private var reConfirmIndex_:Int  = 0
-    private var resultValue_:Int     = 0
+    private var comfirmIndex_:Int       = 0
+    private var reConfirmIndex_:Int     = 0
+    private var resultValue_:Int        = 0
+    private var nearResultIndex_:Int    = 0
+    private var farResultIndex_:Int     = 0
+    private var farIndex_:Int           = 0
+    private var nearIndex_:Int          = 0
     
-    private var newestPrescriptionR_S_:Float = 0.0
-    private var newestPrescriptionR_C_:Float = 0.0
-    private var newestPrescriptionL_S_:Float = 0.0
-    private var newestPrescriptionL_C_:Float = 0.0
+    private var newestPrescriptionR_S_:Float    = 0.0
+    private var newestPrescriptionR_C_:Float    = 0.0
+    private var newestPrescriptionR_Add_:Float  = 0.0
+    private var newestPrescriptionL_S_:Float    = 0.0
+    private var newestPrescriptionL_C_:Float    = 0.0
+    private var newestPrescriptionL_Add_:Float  = 0.0
 
-    private var currentPrescriptionR_S_:Float = 0.0
-    private var currentPrescriptionR_C_:Float = 0.0
-    private var currentPrescriptionL_S_:Float = 0.0
-    private var currentPrescriptionL_C_:Float = 0.0
+    private var currentPrescriptionR_S_:Float   = 0.0
+    private var currentPrescriptionR_C_:Float   = 0.0
+    private var currentPrescriptionR_Add_:Float = 0.0
+    private var currentPrescriptionL_S_:Float   = 0.0
+    private var currentPrescriptionL_C_:Float   = 0.0
+    private var currentPrescriptionL_Add_:Float = 0.0
 
+    private var lensTypeBrand_:Int      = 0
+    private var lensTypeProduct_:Int    = 0
+    
     // アクセサー
     func getGlassType() -> GlassType {
         return glassType_
@@ -98,12 +124,44 @@ final class DataServer {
         lensType_ = newLensType
     }
 
+    func getCountry() -> Country {
+        return country_
+    }
+    
+    func setCountry(_ newStatus:Country) {
+        country_ = newStatus
+    }
+    
+    func getTestKind() -> TestKind {
+        return testKind_
+    }
+    
+    func setTestKind(_ newStatus: TestKind) {
+        testKind_ = newStatus
+    }
+    
     func getSettingPos() -> SettingPos {
         return settingPos_
     }
     
     func setSettingPos(_ newStatus: SettingPos) {
         settingPos_ = newStatus
+    }
+    
+    func getFarIndex() -> Int {
+        return farIndex_
+    }
+    
+    func setFarIndex(_ newIndex:Int) {
+        farIndex_ = newIndex
+    }
+    
+    func getNearIndex() -> Int {
+        return nearIndex_
+    }
+    
+    func setNearIndex(_ newIndex:Int) {
+        nearIndex_ = newIndex
     }
     
     func getNewestPrescriptionR_S() -> Float {
@@ -122,6 +180,14 @@ final class DataServer {
         newestPrescriptionR_C_ = newestPrescriptionR_C
     }
 
+    func getNewestPrescriptionR_Add() -> Float {
+        return newestPrescriptionR_Add_
+    }
+    
+    func setNewestPrescriptionR_Add(_ newestPrescriptionR_Add:Float) {
+        newestPrescriptionR_Add_ = newestPrescriptionR_Add
+    }
+    
     func getNewestPrescriptionL_S() -> Float {
         return newestPrescriptionL_S_
     }
@@ -136,6 +202,14 @@ final class DataServer {
     
     func setNewestPrescriptionL_C(_ newestPrescriptionL_C:Float) {
         newestPrescriptionL_C_ = newestPrescriptionL_C
+    }
+
+    func getNewestPrescriptionL_Add() -> Float {
+        return newestPrescriptionL_Add_
+    }
+    
+    func setNewestPrescriptionL_Add(_ newestPrescriptionL_Add:Float) {
+        newestPrescriptionL_Add_ = newestPrescriptionL_Add
     }
 
     func getCurrentPrescriptionR_S() -> Float {
@@ -154,6 +228,14 @@ final class DataServer {
         currentPrescriptionR_C_ = currentPrescriptionR_C
     }
     
+    func getCurrentPrescriptionR_Add() -> Float {
+        return currentPrescriptionR_Add_
+    }
+    
+    func setCurrentPrescriptionR_Add(_ currentPrescriptionR_Add:Float) {
+        currentPrescriptionR_Add_ = currentPrescriptionR_Add
+    }
+    
     func getCurrentPrescriptionL_S() -> Float {
         return currentPrescriptionL_S_
     }
@@ -170,6 +252,14 @@ final class DataServer {
         currentPrescriptionL_C_ = currentPrescriptionL_C
     }
 
+    func getCurrentPrescriptionL_Add() -> Float {
+        return currentPrescriptionL_Add_
+    }
+    
+    func setCurrentPrescriptionL_Add(_ currentPrescriptionL_Add:Float) {
+        currentPrescriptionL_Add_ = currentPrescriptionL_Add
+    }
+    
     func getConfirmIndex() -> Int {
         return comfirmIndex_
     }
@@ -251,6 +341,353 @@ final class DataServer {
         resultValue_ = newValue
     }
 
+    func setNearResultIndex(_ newValue:Int) {
+        nearResultIndex_ = newValue
+    }
+    
+    func getNearResultValue() -> Int {
+        
+        switch nearResultIndex_ {
+        case 0:
+            return -5
+        case 1:
+            return -4
+        case 2:
+            return -4
+        case 3:
+            return -3
+        case 4:
+            return -3
+        case 5:
+            return -2
+        case 6:
+            return -1
+        case 7:
+            return 0
+        case 8:
+            return 1
+        case 9:
+            return 2
+        case 10:
+            return 2
+        case 11:
+            return 3
+        case 12:
+            return 3
+        case 13:
+            return 4
+        case 14:
+            return 4
+        case 15:
+            return 5
+        case 16:
+            return 5
+        default:
+            return -99
+        }
+    }
+    
+    func setFarResultIndex(_ newValue:Int) {
+        farResultIndex_ = newValue
+    }
+    
+    func getFarResultValue() -> Int {
+        
+        switch farResultIndex_ {
+        case 0:
+            return -5
+        case 1:
+            return -4
+        case 2:
+            return -4
+        case 3:
+            return -3
+        case 4:
+            return -3
+        case 5:
+            return -2
+        case 6:
+            return -1
+        case 7:
+            return 0
+        case 8:
+            return 1
+        case 9:
+            return 2
+        case 10:
+            return 2
+        case 11:
+            return 3
+        case 12:
+            return 3
+        case 13:
+            return 4
+        case 14:
+            return 4
+        case 15:
+            return 5
+        case 16:
+            return 5
+        default:
+            return -99
+        }
+    }
+    
+    func setLensTypeParam(_ brand:Int, product:Int) {
+        lensTypeBrand_   = brand
+        lensTypeProduct_ = product
+    }
+    
+    func getLensTypeParam() -> Int {
+        
+        if (country_ == Country.Japan) {
+            
+            switch lensTypeBrand_ {
+                
+            case 0: // Nikon
+                switch lensTypeProduct_ {
+                case 0: // SEEPROUD Ai
+                    return 11
+                case 1: // Ai Original
+                    return 12
+                case 2: // Ai cyber
+                    return 13
+                case 3: // Ai Entry
+                    return 14
+                case 4: // Ai Smooth
+                    return 15
+                case 5: // SEEPROUD High Power
+                    return 16
+                case 6: // Presio High Power
+                    return 17
+                case 7: // Presio Power
+                    return 18
+                case 8: // Presio W
+                    return 19
+                case 9: // Seven
+                    return 20
+                case 10: // 中近タイプ
+                    return 98
+                case 11: // 不明・該当なし
+                    return 10
+                default:
+                    return -99
+                }
+                
+            case 1: // Varlux
+                switch lensTypeProduct_ {
+                case 0: // S 4D
+                    return 32
+                case 1: // S 3D
+                    return 33
+                case 2: // S
+                    return 34
+                case 3: // Definity
+                    return 35
+                case 4: // Physio
+                    return 36
+                case 5: // Comfort
+                    return 37
+                case 6: // 中近タイプ
+                    return 98
+                case 7: // 不明・該当なし
+                    return 31
+                default:
+                    return -99
+                }
+                
+            case 2: // Hoya
+                switch lensTypeProduct_ {
+                case 0: // Rsi
+                    return 47
+                case 1: // Msi/Msv Mild
+                    return 48
+                case 2: // Msi/Msv Harmony
+                    return 49
+                case 3: // Msi/Msv Clarity
+                    return 50
+                case 4: // Lsi/Lsv Harmony
+                    return 51
+                case 5: // Lsi/Lsv Clarity
+                    return 52
+                case 6: // Welna Harmony
+                    return 53
+                case 7: // Welna Clarity
+                    return 54
+                case 8: // SynchroField
+                    return 55
+                case 9: // iD/FD
+                    return 56
+                case 10:// 中近タイプ
+                    return 98
+                case 11:// 不明・該当なし
+                    return 46
+                default:
+                    return -99
+                }
+                
+            case 3: // Seiko
+                switch lensTypeProduct_ {
+                case 0: // SuperiorPX
+                    return 62
+                case 1: // SuperiorP1neo
+                    return 63
+                case 2: // SuperiorP1
+                    return 64
+                case 3: // Pursuit AC-X
+                    return 65
+                case 4: // Pursuit PV
+                    return 66
+                case 5: // Pursuit CV
+                    return 67
+                case 6: // Pursuit CV-X
+                    return 68
+                case 7: // Pursuit NV-X
+                    return 69
+                case 8: // Pursuit NV
+                    return 70
+                case 9: // Pursuit NVマイルド
+                    return 71
+                case 10:// 中近タイプ
+                    return 98
+                case 11:// 不明・該当なし
+                    return 61
+                default:
+                    return -99
+                }
+                
+            case 4: // Tokai
+                switch lensTypeProduct_ {
+                case 0: // ニューログラン
+                    return 77
+                case 1: // グラナス マイルドタイプ
+                    return 78
+                case 2: // グラナス クリアタイプ
+                    return 79
+                case 3: // レゾナスRマイチューン
+                    return 80
+                case 4: // レゾナスR
+                    return 81
+                case 5: // レゾナスフィットR
+                    return 82
+                case 6: // レゾナスマイチューン
+                    return 83
+                case 7: // レゾナス
+                    return 84
+                case 8: // レゾナスフィット
+                    return 85
+                case 9: // グレイス
+                    return 86
+                case 10:// 中近タイプ
+                    return 98
+                case 11:// 不明・該当なし
+                    return 76
+                default:
+                    return -99
+                }
+                
+            case 5: // Zeiss
+                switch lensTypeProduct_ {
+                    
+                case 0: // Indivisual
+                    return 92
+                case 1: // 中近タイプ
+                    return 98
+                case 2: // 不明・該当なし
+                    return 91
+                default:
+                    return -99
+                }
+                
+            default:
+                if (lensTypeBrand_ == 6) {
+                    return 99
+                } else {
+                    return -99
+                }
+            }
+        } else if (country_ == Country.Other) {
+            
+            switch lensTypeBrand_ {
+                
+            case 0: // Nikon
+                switch lensTypeProduct_ {
+                case 0: // SEEMAX MASTER
+                    return 20
+                case 1: // PRESIO MASTER
+                    return 21
+                case 2: // PRESIO POWER
+                    return 22
+                case 3: // PRESIO DIGILIFE Wide
+                    return 23
+                case 4: // PRESIO BALANCE
+                    return 24
+                case 5: // PRESIO i Focus
+                    return 25
+                case 6: // Mid PAL Type
+                    return 98
+                case 7: // Uncertain / NA
+                    return 10
+                default:
+                    return -99
+                }
+                
+            case 1: // Varlux
+                switch lensTypeProduct_ {
+                case 0: // S 4D
+                    return 32
+                case 1: // S 3D
+                    return 33
+                case 2: // S
+                    return 34
+                case 3: // Physio
+                    return 36
+                case 4: // Comfort
+                    return 37
+                case 5: // Mid PAL Type
+                    return 98
+                case 6: // Uncertain / NA
+                    return 31
+                default:
+                    return -99
+                }
+                
+            case 2: // Hoya
+                switch lensTypeProduct_ {
+                case 0: // iD MyStyle V+
+                    return 48
+                case 1: // iD LifeStyle V+
+                    return 49
+                case 2: // Mid PAL Type
+                    return 98
+                case 3: // Uncertain / NA
+                    return 46
+                default:
+                    return -99
+                }
+                
+            case 3: // Zeiss
+                switch lensTypeProduct_ {
+                case 0: // Indivisual
+                    return 92
+                case 1: // Mid PAL Type
+                    return 98
+                case 2: // Uncertain / NA
+                    return 91
+                default:
+                    return -99
+                }
+                
+            default:
+                if (lensTypeBrand_ == 4) {
+                    return 99
+                } else {
+                    return -99
+                }
+            }
+        }
+        return -99
+    }
 }
 
 // EOF
